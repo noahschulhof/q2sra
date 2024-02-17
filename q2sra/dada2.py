80ac61dae0a6894e7978803da1dc2fcf377adad8
Noah Schulhof <noahschulhof2026@u.northwestern.edu>
2024-02-16 20:43:42 -0600

    Fixed trunc_len arg

--- q2sra/dada2.py
+++ q2sra/dada2.py
@@ -1,10 +1,7 @@
 import qiime2.plugins.dada2.actions as dada2_actions
 
-def dada2(study_name, demux, trim_left = 0, trunc_len = None):
-    if trunc_len:
-        table, rep_seqs, stats = dada2_actions.denoise_single(demultiplexed_seqs = demux, trim_left = trim_left, trunc_len = trunc_len)
-    else:
-        table, rep_seqs, stats = dada2_actions.denoise_single(demultiplexed_seqs = demux, trim_left = trim_left)
+def dada2(study_name, demux, trim_left = 0, trunc_len = 0):
+    table, rep_seqs, stats = dada2_actions.denoise_single(demultiplexed_seqs = demux, trim_left = trim_left, trunc_len = trunc_len)
     
     table.save(f'{study_name}_table.qza')
     rep_seqs.save(f'{study_name}_repseqs.qza')
