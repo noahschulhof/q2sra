cbab1029a8391cfe4c67d8cf02596a02a3f640dd
Noah Schulhof <noahschulhof2026@u.northwestern.edu>
2024-02-20 15:05:17 -0600

    Updated tests

--- tests/test_merge.py
+++ tests/test_merge.py
@@ -1,5 +1,4 @@
 import unittest
-import shutil
 import os
 from q2sra.merge_metadata import merge_metadata
 from q2sra.merge_repseqs import merge_repseqs
@@ -7,13 +6,22 @@ from q2sra.merge_tables import merge_tables
 
 class MergeTest(unittest.TestCase):
     def setUp(self):
-        pass
-    
+        os.chdir('tests')
+
 
     def tearDown(self):
-        pass
+        os.remove('sample_proj/sample_proj_table_merged.qza')
+        os.remove('sample_proj/sample_proj_repseqs_merged.qza')
 
+    
+    def test_merge(self):
+        merge_tables('sample_proj')
+        merge_repseqs('sample_proj')
 
+        assert os.path.exists('sample_proj/sample_proj_table_merged.qza')
+        assert os.path.exists('sample_proj/sample_proj_repseqs_merged.qza')
+
+        
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_run.py
+++ tests/test_run.py
@@ -1,6 +1,9 @@
 import unittest
 import shutil
 import os
+import sys
+
+sys.path.append('q2sra')
 from q2sra.proj import Proj
 
 class RunTest(unittest.TestCase):
@@ -13,14 +16,14 @@ class RunTest(unittest.TestCase):
     
 
     def test_paired(self):
-        self.p.run('chang', 'PRJNA601807')
+        self.p.run('chang', 'PRJNA601807', use_deblur = False)
         self.assertEqual(len(os.listdir('chang/fastq-files')), 20)
-        assert os.path.exists('chang/chang_deblur.qza')
+        assert os.path.exists('chang/chang_table.qza')
         assert os.path.exists('chang/chang_repseqs.qza')
         
         self.p.run('arizza', 'PRJNA481425')
         self.assertEqual(len(os.listdir('arizza/fastq-files')), 36)
-        assert os.path.exists('arizza/arizza_deblur.qza')
+        assert os.path.exists('arizza/arizza_table.qza')
         assert os.path.exists('arizza/arizza_repseqs.qza')
     
 
@@ -30,12 +33,12 @@ class RunTest(unittest.TestCase):
 
         self.p.run('chang', 'PRJNA601807')
         self.assertEqual(len(os.listdir('chang/fastq-files')), 8)
-        assert os.path.exists('chang/chang_deblur.qza')
+        assert os.path.exists('chang/chang_table.qza')
         assert os.path.exists('chang/chang_repseqs.qza')
         
-        self.p.run('arizza', 'PRJNA481425')
+        self.p.run('arizza', 'PRJNA481425', use_deblur = False)
         self.assertEqual(len(os.listdir('arizza/fastq-files')), 8)
-        assert os.path.exists('arizza/arizza_deblur.qza')
+        assert os.path.exists('arizza/arizza_table.qza')
         assert os.path.exists('arizza/arizza_repseqs.qza')
 
 
