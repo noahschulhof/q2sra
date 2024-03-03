de6601ae0650a5e7d8e534d2fee084317ba6dcd1
Noah Schulhof <noahschulhof2026@u.northwestern.edu>
2024-03-03 15:48:00 -0600

    Omit invalid metadata

--- q2sra/merge_metadata.py
+++ q2sra/merge_metadata.py
@@ -7,8 +7,11 @@ def merge_metadata(proj_name):
     for root, _, files in os.walk(proj_name):
         for file in files:
             if file.endswith('metadata.txt'):
-                metadata = pd.read_csv(os.path.join(root, file), sep = '\t')
+                try:
+                    metadata = pd.read_csv(os.path.join(root, file), sep = '\t')
 
-                merged_metadata = pd.concat(merged_metadata, metadata)
+                    merged_metadata = pd.concat(merged_metadata, metadata)
+                except:
+                    print(f'Omitting invalid metadata file: {file}')
 
     merged_metadata.to_csv(os.path.join(proj_name, f'{proj_name}_metadata_merged.txt'), sep = '\t')
