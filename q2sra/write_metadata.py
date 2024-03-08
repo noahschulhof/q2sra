57cb99e531306ef34a20701236adabd6beeb7a83
Noah Schulhof <noahschulhof2026@u.northwestern.edu>
2024-03-08 00:01:54 -0600

    Add comma delim option

--- q2sra/write_metadata.py
+++ q2sra/write_metadata.py
@@ -3,13 +3,17 @@ import pandas as pd
 def write_metadata(field_vals, study_name):
     manifest = pd.read_csv(f'{study_name}_manifest.txt', sep = '\t')
 
+    if not 'sample_id' in manifest.columns:
+        manifest = pd.read_csv(f'{study_name}_manifest.txt')
+
     num_samples = manifest.shape[0]
 
-    metadata = pd.DataFrame()
+    try:
+        metadata = pd.DataFrame({'sample-id': manifest['sample-id']})
 
-    metadata['sample-id'] = manifest['sample-id']
-
-    for field, val in field_vals.items():
-        metadata[field] = [val] * num_samples
-    
-    metadata.to_csv(f'{study_name}_metadata.txt', sep = '\t', index = False)
+        for field, val in field_vals.items():
+            metadata[field] = [val] * num_samples
+        
+        metadata.to_csv(f'{study_name}_metadata.txt', sep = '\t', index = False)
+    except:
+        pass
