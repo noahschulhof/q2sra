6240a860a80950742f2d56f2a109c37692c7446b
Noah Schulhof <noahschulhof2026@u.northwestern.edu>
2024-03-27 20:57:25 -0500

    Update description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'q2sra',
     version = '1.0.0',
-    description = 'Fetch, process, analyze, and aggregate microbiome sequencing data with SRA Toolkit and QIIME2.',
+    description = 'Fetch, process, analyze, and aggregate microbiome sequencing data with SRA Toolkit and QIIME 2.',
     author = 'Noah Schulhof',
     author_email = 'nschulhof@u.northwestern.edu',
     packages = find_packages(),
